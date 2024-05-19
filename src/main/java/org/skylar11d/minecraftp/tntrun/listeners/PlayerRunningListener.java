1a44a242c1b1577e7b471a26c0732b6cbf0b71d1
Skylar11D <Skylar11D@aol.com>
2024-05-19 20:01:09 +0300

    fixed threads fatal issue

--- src/main/java/org/skylar11d/minecraftp/tntrun/listeners/PlayerRunningListener.java
+++ src/main/java/org/skylar11d/minecraftp/tntrun/listeners/PlayerRunningListener.java
@@ -1,23 +1,25 @@
 package org.skylar11d.minecraftp.tntrun.listeners;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.jetbrains.annotations.NotNull;
 import org.skylar11d.minecraftp.tntrun.Main;
 import org.skylar11d.minecraftp.tntrun.utilities.assets.events.PlayerRunEvent;
 import org.skylar11d.minecraftp.tntrun.utilities.manager.runner.Runner;
 import org.skylar11d.minecraftp.tntrun.utilities.manager.runner.RunnerManager;
 
+import java.util.concurrent.*;
+
 public class PlayerRunningListener implements Listener {
 
+    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
     @EventHandler
-    public void onRunning(PlayerRunEvent e){
+    public void onRunning(@NotNull PlayerRunEvent e){
         RunnerManager runnerManager = Main.getInstance().getRunnerManager();
         Runner runner = e.getRunner();
-        Player player = e.getRunner().getPlayer();
 
-        runnerManager.getRunners().get(runner).runTaskLater(Main.getPlugin(Main.class), 14);
-
+        pool.execute(() -> runnerManager.getRunners().get(runner).runTaskLater(Main.getPlugin(Main.class), 14));
     }
 
 }
